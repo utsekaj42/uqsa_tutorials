2ec0ca0afcb31bda9f052d6da827a0d756ea5992
Jacob Sturdy <jacob.sturdy@gmail.com>
2017-06-23 11:52:09 +0200

    added chaospy to repo

---
+++ chaospy/__init__.py
@@ -0,0 +1,27 @@
+"""
+Uncertainty Quantifican Toolbox
+===============================
+
+This module contains tools for performing uncertainty
+quantification of models.
+"""
+
+import chaospy.bertran
+import chaospy.chol
+import chaospy.descriptives
+import chaospy.dist
+import chaospy.orthogonal
+import chaospy.poly
+import chaospy.spectral
+import chaospy.quad
+import chaospy.saltelli
+import chaospy.regression
+
+from chaospy.dist import *
+from chaospy.orthogonal import *
+from chaospy.poly import *
+from chaospy.spectral import *
+from chaospy.quad import *
+from chaospy.saltelli import *
+from chaospy.descriptives import *
+from chaospy.regression import *
